0dda332096182ba285334728efa1db03391e22cc
Mati <matias.abarca2018@gmail.com>
2021-12-07 12:48:51 -0300

    Pequeños cambios esteticos

--- src/app/panel/crear/crear.component.ts
+++ src/app/panel/crear/crear.component.ts
@@ -5,6 +5,7 @@ import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/c
 import { map, pluck, switchMap, tap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Producto } from 'src/app/interfaces/producto.interface';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-crear',
@@ -32,7 +33,8 @@ export class CrearComponent implements OnInit, OnDestroy {
                 private fireService: FireService,
                 private storage: AngularFireStorage,
                 private activatedRoute: ActivatedRoute,
-                private router: Router) { }
+                private router: Router,
+                public toastController: ToastController) { }
   
   ngOnDestroy(): void {
     this.editar = false;
@@ -49,12 +51,13 @@ export class CrearComponent implements OnInit, OnDestroy {
       map( id => this.fireService.traerProductoId(id).valueChanges() ),
       switchMap( resp => resp ),//Se subscribe al observable de traerProductos.
       tap<Producto>( result => {
-        const { descripcion, nombre, precio, stock, sku } = result;
+        const { descripcion, nombre, precio, stock, sku, imagen } = result;
         this.miFormulario.get('nombre').setValue(nombre);
         this.miFormulario.get('precio').setValue(precio);
         this.miFormulario.get('stock').setValue(stock);
         this.miFormulario.get('sku').setValue(sku);
         this.miFormulario.get('descripcion').setValue(descripcion);
+        this.miFormulario.get('imagen').setValue(imagen);
         
       })
     )
@@ -62,8 +65,13 @@ export class CrearComponent implements OnInit, OnDestroy {
   }
 
   async crearProducto(){
+    const toast = await this.toastController;
+    
+
     if( !this.editar ){
       if( this.miFormulario.invalid ){
+        const toastErr = toast.create({ message: 'Debe ingresar todos los campos', duration:1500, color:'danger' } );
+         (await toastErr).present();
         return;
       }
        const task =  await this.ref.put( this.file );//Aqui se ejecuta el subir la imagen.
@@ -75,13 +83,17 @@ export class CrearComponent implements OnInit, OnDestroy {
           this.miFormulario.reset();
         } 
       } );
+      const toastOk = toast.create({ message: 'Producto guardado', duration:1200, color:'success' } );
+      (await toastOk).present();
     }else{
       if( !this.router.url.includes('editar') ){
         return;
       }
-
-      this.fireService.traerProductoId(this.id).update( this.miFormulario.value );
       
+      this.fireService.traerProductoId(this.id).update( this.miFormulario.value );
+      const toastOk = toast.create({ message: 'Producto editado', duration:1200, color:'success' } );
+      (await toastOk).present();
+      this.miFormulario.reset();
     }
   }
   obtenerImagen( event ){
